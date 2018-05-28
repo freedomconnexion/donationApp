6db9f74f1e4ae8a227d4144b2fab81f08c427a02
Justin Hildebrandt <jhilde@gmail.com>
2018-05-28 11:46:05 -0400

    Adding vuelidate to Vue instance

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Vuetify from 'vuetify'
+import Vuelidate from 'vuelidate'
 import Vuex from 'vuex'
 import 'vuetify/dist/vuetify.css'
 import store from './store'
 
 Vue.use(Vuetify)
 Vue.use(Vuex)
+Vue.use(Vuelidate)
 
 new Vue({
   el: '#app',
